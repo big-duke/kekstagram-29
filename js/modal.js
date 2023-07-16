f4506316ad7ae201789d019f74227b8210c603e5
alexey kozlov <alexey.kozlov@live.ru>
2023-07-16 23:18:56 +0300

    8.15. Открывается и закрывается (часть 2)

--- js/modal.js
+++ js/modal.js
@@ -5,11 +5,13 @@ const pictureElement = document.querySelector('.big-picture__img > img');
 const descriptionElement = document.querySelector('.social__caption');
 const likesCountElement = document.querySelector('.likes-count');
 
-const commentsCountContainerElement = document.querySelector('.social__comment-count');
 const commentsLoaderButton = document.querySelector('.social__comments-loader');
 const commentsCountElement = document.querySelector('.comments-count');
+const displayedCommentsCountElement = document.querySelector('.displayed__comments-count');
 const commentsContainer = document.querySelector('.social__comments');
 const commentTemplate = document.querySelector('#social__comment').content;
+
+let displayCommentsCount = 0;
 
 const createComment = ({ avatar, name, message }) => {
   const comment = commentTemplate.cloneNode(true);
@@ -20,21 +22,30 @@ const createComment = ({ avatar, name, message }) => {
 };
 
 const renderComments = (comments) => {
+  displayCommentsCount += 5;
+
+  if (displayCommentsCount >= comments.length) {
+    commentsLoaderButton.classList.add('hidden');
+    displayCommentsCount = comments.length;
+  } else {
+    commentsLoaderButton.classList.remove('hidden');
+  }
 
   commentsContainer.innerHTML = '';
   const fragment = document.createDocumentFragment();
-  comments.forEach((item) => {
+
+  comments.slice(0,displayCommentsCount).forEach((item) => {
     const comment = createComment(item);
     fragment.append(comment);
   });
   commentsContainer.append(fragment);
+  displayedCommentsCountElement.textContent = displayCommentsCount;
 
 };
+
 export const displayPictureModal = ({ url, description, likes, comments }) => {
   pictureModal.classList.remove('hidden');
   bodyElelement.classList.add('modal-open');
-  commentsCountContainerElement.classList.add('hidden');
-  commentsLoaderButton.classList.add('hidden');
 
   pictureElement.src = url;
   pictureElement.alt = description;
@@ -45,16 +56,18 @@ export const displayPictureModal = ({ url, description, likes, comments }) => {
 
   renderComments(comments);
 
-  document.addEventListener('keydown', handleDocumentKeyDown);
+  document.addEventListener('keydown', documentKeyDownHandler);
+  commentsLoaderButton.addEventListener('click', () => renderComments(comments));
 };
 
 const hideModal = () => {
   pictureModal.classList.add('hidden');
   bodyElelement.classList.remove('modal-open');
-  document.removeEventListener('keydown', handleDocumentKeyDown);
+  document.removeEventListener('keydown', documentKeyDownHandler);
+  displayCommentsCount = 0;
 };
 
-function handleDocumentKeyDown(e) {
+function documentKeyDownHandler(e) {
   if (e.key === 'Escape') {
     e.preventDefault();
     hideModal();
